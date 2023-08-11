31749059e3b8481525d57d2b6a4c5c12f912228d
Robson-Luna-BR <robson.luna@outlook.com>
2023-08-11 06:46:16 -0300

    feat: Adiciona Schema e interface de update de Ad, e refatora alguams linahs do código update no controler

--- src/schemas/salesAd.schemas.ts
+++ src/schemas/salesAd.schemas.ts
@@ -1,26 +1,24 @@
-import { z } from "zod"
-import { FuelType } from "../entities/ads.entity"
+import { z } from "zod";
+import { FuelType } from "../entities/ads.entity";
 
 const salesAdSchema = z.object({
-    id: z.string(),
-    brand: z.string(),
-    model: z.string(),
-    year: z.date(),
-    fuel: z.nativeEnum(FuelType),
-    kilometers: z.number(),
-    color: z.string(),
-    good_deal: z.boolean().default(false),
-    price: z.number(),
-    description: z.string(),
-    published: z.boolean().default(false),
-})
+  id: z.string(),
+  brand: z.string(),
+  model: z.string(),
+  year: z.date(),
+  fuel: z.nativeEnum(FuelType),
+  kilometers: z.number(),
+  color: z.string(),
+  good_deal: z.boolean().default(false),
+  price: z.number(),
+  description: z.string(),
+  published: z.boolean().default(false),
+});
 
 const salesAdRequestSchema = salesAdSchema.omit({
-    id:true,
-    good_deal:true,
-    published:true,
-})
+  id: true,
+  good_deal: true,
+  published: true,
+});
 
-
-
-export { salesAdSchema, salesAdRequestSchema }
+export { salesAdSchema, salesAdRequestSchema };
