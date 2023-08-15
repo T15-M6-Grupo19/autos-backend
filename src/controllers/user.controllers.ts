af40d2be977d64a38aba6328f7f87e78126307ee
Arthur Silva Mendonça <arthu91@gmail.com>
2023-08-15 17:50:53 -0300

    feat: criada rota de GET para listar usuários e usuário com respectivos Ads

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from 'express';
-import { createUserService } from '../services/user/createUser.service';
-import { deleteUserService } from '../services/user/deleteUser.service';
-import { updateUserService } from '../services/user/updateUser.service';
+import { Request, Response } from "express";
+import { createUserService } from "../services/user/createUser.service";
+import { deleteUserService } from "../services/user/deleteUser.service";
+import { updateUserService } from "../services/user/updateUser.service";
+import getUsersService from "../services/user/getUsers.service";
+import getUserByIdService from "../services/user/getUserById.service";
 
 export const createUserController = async (
   req: Request,
@@ -30,4 +32,23 @@ export const deleteUserController = async (
   await deleteUserService(user);
 
   return res.status(204).send();
+};
+
+export const getUsersController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const users = await getUsersService();
+
+  return res.status(200).json(users);
+};
+
+export const getUserByIdController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const user = res.locals.user;
+  const getUser = await getUserByIdService(user);
+
+  return res.status(200).json(getUser);
 };
