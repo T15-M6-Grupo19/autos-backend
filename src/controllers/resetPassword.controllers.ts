8e1a94934335b6841a5f1a853e8526408d47174e
iurych <iury.vibe@gmail.com>
2023-08-24 19:09:01 -0300

    feat: to pull 24082023 1908

--- src/controllers/resetPassword.controllers.ts
+++ src/controllers/resetPassword.controllers.ts
@@ -1,24 +1,22 @@
-import { Request, Response } from "express";
-import { resetPasswordService } from "../services/password/resetPassword.service";
-import { sendResetEmailService } from "../services/password/sendResetEmail.service";
+import { Request, Response } from 'express';
+import { resetPasswordService } from '../services/password/resetPassword.service';
+import { sendResetEmailService } from '../services/password/sendResetEmail.service';
 
-const sendResetEmailController = async (req: Request, res: Response) =>{
-    const { email } = req.body
+const sendResetEmailController = async (req: Request, res: Response) => {
+  const { email } = req.body;
 
-    await sendResetEmailService(email)
+  await sendResetEmailService(email);
 
-    return res.json({message: "token sent!"})
-}
+  return res.json({ message: 'token sent!' });
+};
 
-const resetPasswordController = async (req: Request, res: Response) =>{
-    const { password } = req.body
-    const { token } = req.params
+const resetPasswordController = async (req: Request, res: Response) => {
+  const { password } = req.body;
+  const { token } = req.params;
 
-    await resetPasswordService(password, token)
+  await resetPasswordService(password, token);
 
-    return res.json({message: "password changed!"})
-}
+  return res.json({ message: 'password changed!' });
+};
 
-
-
-export { sendResetEmailController, resetPasswordController }
+export { sendResetEmailController, resetPasswordController };
