b699648152341872dc0ccb437bfe04c8b170e9c3
iurych <iury.vibe@gmail.com>
2023-09-05 21:27:43 -0300

    feat: add deploy config

---
+++ src/migrations/1693579406088-createUsers.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsers1693579406088 implements MigrationInterface {
+    name = 'CreateUsers1693579406088'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "comments" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "comment_text" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, "adId" uuid, CONSTRAINT "PK_8bf68bc960f2b69e818bdb90dcb" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "photos" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "photo_url" character varying NOT NULL, "adId" uuid, CONSTRAINT "PK_5220c45b8e32d49d767b9b3d725" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."ads_fuel_enum" AS ENUM('flex', 'hibrido', 'eletrico')`);
+        await queryRunner.query(`CREATE TABLE "ads" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "brand" character varying NOT NULL, "model" character varying NOT NULL, "year" character varying NOT NULL, "fuel" "public"."ads_fuel_enum" NOT NULL DEFAULT 'flex', "kilometers" integer NOT NULL, "color" character varying NOT NULL, "good_deal" boolean NOT NULL DEFAULT false, "price" integer NOT NULL, "description" character varying NOT NULL, "published" boolean NOT NULL DEFAULT false, "userId" uuid, CONSTRAINT "PK_a7af7d1998037a97076f758fc23" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."users_account_type_enum" AS ENUM('anunciante', 'comprador')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying(120) NOT NULL, "CPF" character varying NOT NULL, "mobile" character varying NOT NULL, "birth_date" date NOT NULL, "description" character varying(50), "ZIP_code" character varying NOT NULL, "state" character varying NOT NULL, "city" character varying NOT NULL, "street" character varying NOT NULL, "number" character varying NOT NULL, "additional_details" character varying(40), "account_type" "public"."users_account_type_enum" NOT NULL DEFAULT 'comprador', "createdAt" date NOT NULL DEFAULT now(), "updatedAt" date NOT NULL DEFAULT now(), "deletedAt" date, "reset_token" character varying, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_f1ca1f4ecd02811179c26b6d664" FOREIGN KEY ("adId") REFERENCES "ads"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "photos" ADD CONSTRAINT "FK_b1426233b7de5c32c767984fa91" FOREIGN KEY ("adId") REFERENCES "ads"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "ads" ADD CONSTRAINT "FK_e72da72588dc5b91427a9adda71" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "ads" DROP CONSTRAINT "FK_e72da72588dc5b91427a9adda71"`);
+        await queryRunner.query(`ALTER TABLE "photos" DROP CONSTRAINT "FK_b1426233b7de5c32c767984fa91"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_f1ca1f4ecd02811179c26b6d664"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_account_type_enum"`);
+        await queryRunner.query(`DROP TABLE "ads"`);
+        await queryRunner.query(`DROP TYPE "public"."ads_fuel_enum"`);
+        await queryRunner.query(`DROP TABLE "photos"`);
+        await queryRunner.query(`DROP TABLE "comments"`);
+    }
+
+}
