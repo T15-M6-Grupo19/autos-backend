af1de1e90368101d37173d809dd32801fcba055c
iurych <iury.vibe@gmail.com>
2023-09-06 14:18:33 -0300

    feat: adiciona lógica para PORT

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import app from './app'
 import { AppDataSource } from './data-source'
 
 
-const port = 3000
+
 AppDataSource.initialize()
+
     
     .then(() => {
         console.log('Database connected!')
+        const port =  process.env.PORT || 3000
+
         app.listen(port, () => {
             console.log(`Server running on port ${port}`)
         })
